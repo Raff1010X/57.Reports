06a17ae9953487c2797b48257e4bdd5097c83f1f
Rafał Kowalczyk <kowalczykr@trend-glass.local>
2023-02-16 14:02:34 +0100

    add: redux, datastructure

---
+++ src/data/structure.js
@@ -0,0 +1,41 @@
+const user = {
+    email: '',
+    password: '',
+}
+
+const department = {
+    name: '',
+};
+
+const receivers = {
+    department: '',
+    email: '',
+    password: '',
+} 
+
+const report = {
+    header: '',
+    footer: '',
+    title: '',
+    number: '',
+    date_audit: '',
+    date_report: '',
+    departments: '',
+    auditors: '',
+    receivers: '',
+};
+
+const page = {
+    report_number: '',
+    date_audit: '',
+    department: '',
+    place: '',
+    description: '',
+    type: '',
+    foto_1: '',
+    foto_2: '',
+    foto_3: '',
+    foto_4: '',
+    status: '',
+    date_status: '',
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,18 @@ import '@/styles/colors.css';
 import '@/styles/globals.css';
 import '@/styles/Navbar.css';
 
+import { Provider } from 'react-redux';
+import { store } from '../redux/store';
 import type { AppProps } from 'next/app';
 import { fontInter } from '@/assets/fonts/fontLoader';
 import Layout from '@/components/layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <Layout className={`${fontInter.className} layout`}>
-            <Component {...pageProps} />
-        </Layout>
+        <Provider store={store}>
+            <Layout className={`${fontInter.className} layout`}>
+                <Component {...pageProps} />
+            </Layout>
+        </Provider>
     );
 }
