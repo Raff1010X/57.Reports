8c72f0be60c606579dff82a10237f4f29ca49afe
Rafał Kowalczyk <kowalczykr@trend-glass.local>
2023-02-16 12:16:49 +0100

    update: RWD burger menu

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,24 +5,40 @@ import Logo from './Logo';
 import Theme from './Theme';
 import { useState } from 'react';
 
+interface MenuActive {
+    isActive: boolean;
+    className: string;
+}
+
+const initialMenuActive = {
+    isActive: false,
+    className: 'burger-menu',
+};
+
 export default function Navbar() {
-    const [menuActive, setMenuActive] = useState(false);
-    const [className, setClassName] = useState('burger-menu');
+    const [menuActive, setMenuActive] = useState(initialMenuActive);
 
     function handleClickBurger() {
-        if (!menuActive) setClassName('burger-menu burger-menu--active');
-        if (menuActive) setClassName('burger-menu burger-menu--inactive');
-        setMenuActive((prevState: boolean) => !prevState);
+        setMenuActive((prevState: MenuActive) => {
+            const className = prevState.isActive
+                ? 'burger-menu burger-menu--inactive'
+                : 'burger-menu burger-menu--active';
+            const newMenuActive = { isActive: !prevState.isActive, className };
+            return newMenuActive;
+        });
     }
 
     return (
         <nav className="navbar">
             <Logo handleClick={handleClickBurger} />
-            <div className={className}>
+            <div className={menuActive.className}>
                 <NavLinks handleClick={handleClickBurger} />
                 <Theme handleClick={handleClickBurger} />
             </div>
-            <Burger menuActive={menuActive} handleClick={handleClickBurger} />
+            <Burger
+                menuActive={menuActive.isActive}
+                handleClick={handleClickBurger}
+            />
         </nav>
     );
 }
