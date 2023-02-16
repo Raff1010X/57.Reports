ccbda429a3050ac39f28d040e241b27423645034
Rafał Kowalczyk <kowalczykr@trend-glass.local>
2023-02-16 15:02:14 +0100

    add: redux asyncDispatch test

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,22 +1,27 @@
-import {
-    Action,
-    configureStore,
-    ThunkAction,
-  } from '@reduxjs/toolkit';
-  
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import asyncDispatchMiddleware from './asyncDispatchMiddleware'
+
 import userReducer from './slices/user/userSlice';
-  
-  export const store = configureStore({
-    reducer: {
-      user: userReducer
-    },
-  });
-  
-  export type AppDispatch = typeof store.dispatch;
-  export type RootState = ReturnType<typeof store.getState>;
-  export type AppThunk<ReturnType = void> = ThunkAction<
-     ReturnType,
-     RootState,
-     unknown,
-     Action<string>
-   >;
+
+const reducer = {
+    user: userReducer,
+}
+
+const middleware = (getDefaultMiddleware: () => any) => [
+    ...getDefaultMiddleware(),
+    ...[asyncDispatchMiddleware],
+];
+
+export const store = configureStore({
+    reducer,
+    middleware,
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>;
