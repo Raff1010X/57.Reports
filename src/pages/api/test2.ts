67d9108a8518f178dd96de0c33db7b448697ab55
Rafał Kowalczyk <kowalczykr@trend-glass.local>
2023-02-27 11:45:58 +0100

    commit before merge

--- src/pages/api/test2.ts
+++ src/pages/api/test2.ts
@@ -1,20 +1,18 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
-import createRouter from "next-connect";
 import nextConnect from 'next-connect';
 
-const router = createRouter<NextApiRequest, NextApiResponse>();
+
+
 
 const handler = nextConnect<NextApiRequest, NextApiResponse>();
 
-const MyMiddleware = async (req: NextApiRequest, res: NextApiResponse, next: NextApiHandler) => {
+const myMiddleware = async (req: NextApiRequest, res: NextApiResponse, next: NextApiHandler) => {
     console.log("ok");
-
-    next(req, res);
+    next(req, res)
+    res.end();
 };
 
-handler.use(MyMiddleware);
-
-handler.get((req: NextApiRequest, res: NextApiResponse) => {
+handler.use("/",myMiddleware).get((req: NextApiRequest, res: NextApiResponse) => {
     console.log("second")
     res.status(200).json({response: "ff"});
 })
