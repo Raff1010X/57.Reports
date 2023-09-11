a860b1f14f0580a7803b326d54ef3d54f6cd7c6b
Rafał Kowalczyk <raff1410X@gmail.com>
2023-09-11 20:33:04 +0200

    update: tests project API

--- src/tests/api/project.test.ts
+++ src/tests/api/project.test.ts
@@ -3,7 +3,6 @@ import request from 'supertest';
 
 const app = 'http://localhost:3000';
 
-
 function statusSuccess(response: any) {
     expect(response.body).toHaveProperty('status');
     expect(response.body).toHaveProperty('message');
@@ -130,6 +129,132 @@ describe('Test the project api', () => {
     test('GET /api/project/:id', (done) => {
         request(app)
             .get(`/api/project/${id}`)
+            .then((response) => {
+                expect(response.body).toHaveProperty('status');
+                expect(response.body).toHaveProperty('message');
+                expect(response.body).not.toHaveProperty('data');
+                expect(response.statusCode).toBe(404);
+                expect(response.body.status).toBe('error');
+                expect(response.body.message).toBe('Error occurred while processing document(s) request');
+
+                done();
+            });
+    });
+    
+
+});
+
+describe('Test the project api with :projectID', () => {
+
+    // send a POST request to the api/project route
+    let id = 1;
+    test('POST /api/project', (done) => {
+        request(app)
+            .post('/api/project')
+            .send({
+                name: 'test project request',
+            })
+            .then((response) => {
+                statusSuccess(response);
+                instanceOf(response);
+                expect(response.body.message).toBe('Document(s) created successfully');
+                expect(response.body.data.name).toBe('test project request');
+                id = response.body.data.id;
+
+                done();
+            });
+    });
+
+    // send a GET request to the api/project route
+    test('GET /api/project', (done) => {
+        request(app)
+            .get('/api/project')
+            .then((response) => {
+                statusSuccess(response);
+                expect(response.body.message).toBe('Document(s) retrieved successfully');
+                expect(response.body.data).toBeInstanceOf(Array);
+                expect(response.body.data).toEqual(expect.arrayContaining([
+                    expect.objectContaining({
+                        name: 'test project request',
+                    }),
+                ]));
+
+                done();
+            });
+    });
+
+    // send a GET request to the api/:projectID route
+    test('GET /api/:projectID', (done) => {
+        request(app)
+
+            .get(`/api/${id}`)
+            .then((response) => {
+                statusSuccess(response);
+                instanceOf(response);
+                expect(response.body.message).toBe('Document(s) retrieved successfully');
+                expect(response.body.data.name).toBe('test project request');
+                expect(response.body.data.id).toBe(id);
+
+                done();
+            });
+    });
+
+    // send a PUT request to the api/:projectID route
+    test('PUT /api/:projectID', (done) => {
+        request(app)
+            .put(`/api/${id}`)
+            .send({
+                name: 'test project request updated',
+            })
+            .then((response) => {
+                statusSuccess(response);
+                instanceOf(response);
+                expect(response.body.message).toBe('Document(s) updated successfully');
+                expect(response.body.data.name).toBe('test project request updated');
+                expect(response.body.data.id).toBe(id);
+
+                done();
+            });
+    });
+
+        // send a GET request to the api/project route
+        test('GET /api/project', (done) => {
+            request(app)
+                .get('/api/project')
+                .then((response) => {
+                    statusSuccess(response);
+                    expect(response.body.message).toBe('Document(s) retrieved successfully');
+                    expect(response.body.data).toBeInstanceOf(Array);
+                    expect(response.body.data).toEqual(expect.arrayContaining([
+                        expect.objectContaining({
+                            name: 'test project request updated',
+                        }),
+                    ]));
+    
+                    done();
+                });
+        });
+
+    // send a DELETE request to the api/:projectID route
+    test('DELETE /api/:projectID', (done) => {
+        request(app)
+
+            .delete(`/api/${id}`)
+            .then((response) => {
+                statusSuccess(response);
+                instanceOf(response);
+                expect(response.body.message).toBe('Document(s) deleted successfully');
+                expect(response.body.data.name).toBe('test project request updated');
+                expect(response.body.data.id).toBe(id);
+                
+                done();
+            });
+    });
+
+    // send a GET request to the api/project:id route to check if the project was deleted
+    test('GET /api/:projectID', (done) => {
+        request(app)
+            .get(`/api/${id}`)
             .then((response) => {
                 expect(response.body).toHaveProperty('status');
                 expect(response.body).toHaveProperty('message');
